4d4d7fc72cc7b47ac7b87df9c0ce5d9ddc830d44
[barak-ish] <[mubarakishola2005@gmail.com]>
2023-01-19 11:43:18 +0100

    added a python file

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("Hello RAIN")
+a=2
+b=4
+c=36
+
+answer1 = (-b+((b**2)-(4*a*c))**(1/2))/(2*a)                                   
+answer2 = (-b-((b**2)-(4*a*c))**(1/2))/(2*a)
+
+print((answer1))
+print((answer2))
